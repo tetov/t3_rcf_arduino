655ad7281e34c03807fa7497e93fb809dc1f0d9f
tetov <anton@tetov.se>
2020-07-29 13:05:12 +0200

    Experiment setup

--- laser-tof-VL53L1X-reading/laser-tof-VL53L1X-reading.ino
+++ laser-tof-VL53L1X-reading/laser-tof-VL53L1X-reading.ino
@@ -1,11 +1,15 @@
 #include <Wire.h>
 #include <VL53L1X.h>
 
+#define BAUD_RATE 115200
+#define TIMING_BUDGET 100000 // in microseconds
+#define DISTANCE_MODE VL53L1X::Short // Short, Medium, Long
+
 VL53L1X sensor;
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(BAUD_RATE);
   Wire.begin();
   Wire.setClock(400000); // use 400 kHz I2C
 
@@ -15,10 +19,11 @@ void setup()
     Serial.println("Failed to detect and initialize sensor!");
     while (1);
   }
-  //sensor.setDistanceMode(VL53L1X::Long);
-  sensor.setDistanceMode(VL53L1X::Short);
-  sensor.setMeasurementTimingBudget(33000);
+  sensor.setDistanceMode(DISTANCE_MODE);
+  sensor.setMeasurementTimingBudget(TIMING_BUDGET);
+
   sensor.startContinuous(15);
+
   Serial.println("New program");
 }
 
